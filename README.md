b12aaf99df2ba9c24bfeb22f2cd75ce94b5ff5c1
Guillermo Galizzi <galizzi.guillermo@gmail.com>
2016-09-28 06:55:26 -0300

    Se implementa SolicitudShow

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Back-end para la app Donemos.
 
 ###Como usar
 
-1. Cloná
+1. Cloná.
 
-2. Ejecutá `mongod`
+2. Ejecutá `mongod`.
 
 3. Usá [Gin](https://github.com/codegangsta/gin) para live-reload.
--- controlador/solicitud.go
+++ controlador/solicitud.go
@@ -8,11 +8,10 @@ import (
 	"net/http"
 
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 
 	"github.com/flioh/DonemosApi/modelos"
 	"github.com/gorilla/mux"
-
-	"gopkg.in/mgo.v2/bson"
 )
 
 // Solicitud es el controlador de solicitudes, contiene referencia a la sesion de mongodb
@@ -25,14 +24,14 @@ func NewSolicitud(s *mgo.Session) *Solicitud {
 	return &Solicitud{s}
 }
 
-func (c *Solicitud) getDb() *mgo.Database {
-	return c.session.DB("donemos")
+func (c *Solicitud) collection() *mgo.Collection {
+	return c.session.DB("donemos").C("solicitudes")
 }
 
 func (c Solicitud) SolicitudIndex(w http.ResponseWriter, r *http.Request) {
 	var solicitudes modelo.Solicitudes
 
-	c.getDb().C("solicitudes").Find(nil).All(&solicitudes)
+	c.collection().Find(nil).All(&solicitudes)
 
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
@@ -45,8 +44,22 @@ func (c Solicitud) SolicitudIndex(w http.ResponseWriter, r *http.Request) {
 
 func (c Solicitud) SolicitudShow(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	id := vars["solicitudId"]
-	fmt.Fprintln(w, "Solicitud Id:", id)
+	id := bson.ObjectIdHex(vars["solicitudId"])
+
+	fmt.Println("Buscando id: ", id)
+
+	var solicitud modelo.Solicitud
+	if err := c.collection().FindId(id).One(&solicitud); err != nil {
+		// Handle not found
+		w.WriteHeader(204)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(solicitud); err != nil {
+		panic(err)
+	}
 }
 
 func (c Solicitud) SolicitudCreate(w http.ResponseWriter, r *http.Request) {
@@ -65,7 +78,7 @@ func (c Solicitud) SolicitudCreate(w http.ResponseWriter, r *http.Request) {
 
 	s.SolicitudId = bson.NewObjectId()
 
-	c.getDb().C("solicitudes").Insert(s)
+	c.collection().Insert(s)
 
 	sj, _ := json.Marshal(s)
 
