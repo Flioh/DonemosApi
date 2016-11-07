a11bb74b66056a67ceddd6efdcf21ff9a8db41e3
Guillermo Galizzi <galizzi.guillermo@gmail.com>
2016-11-07 06:48:15 -0300

    Refactor router.go, GoFmt

--- router/router.go
+++ router/router.go
@@ -2,46 +2,7 @@ package router
 
 import (
 	"net/http"
-	"github.com/flioh/DonemosApi/controlador"
-	"github.com/flioh/DonemosApi/helper"
-	"github.com/gorilla/mux"
-)
 
-func New(controladorSolicitud *controlador.Solicitud,
-	controladorProvincia *controlador.Provincia,
-	controladorLocalidad *controlador.Localidad) *mux.Router {
-	router := mux.NewRouter().StrictSlash(true)
-
-	for _, ruta := range GetRutas(controladorSolicitud, controladorProvincia, controladorLocalidad) {
-		var handler http.Handler
-		handler = helper.Logger(ruta.Handler, ruta.Nombre)
-
-		rutaMux := router.
-			Methods(ruta.Metodo).
-			Name(ruta.Nombre).
-			Path(ruta.Patron).
-			Handler(handler)
-
-		if ruta.Patron == "POST" || ruta.Patron == "PUT" || ruta.Patron == "OPTIONS" {
-			rutaMux.Headers("Content-Type", "application/json")
-			rutaMux.Headers("Access-Control-Allow-Origin", "*")
-			rutaMux.Headers("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE, OPTIONS")
-			rutaMux.Headers("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-		}
-	}
-
-	return router
-}
-
-
-
-
-
-/*package router
-
-import (
-	"net/http"
-	"fmt"
 	jwtm "github.com/auth0/go-jwt-middleware"
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/flioh/DonemosApi/controlador"
@@ -65,25 +26,11 @@ func New(controladorSolicitud *controlador.Solicitud,
 			Path(ruta.Patron)
 
 		if ruta.Patron == "POST" || ruta.Patron == "PUT" || ruta.Patron == "OPTIONS" {
-			fmt.Println(ruta.Patron)
-			rutaMux.Headers("Content-Type", "application/json")
-			rutaMux.Headers("Access-Control-Allow-Origin", "*")
-			rutaMux.Headers("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE, OPTIONS")
-			rutaMux.Headers("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+			setHeaders(rutaMux)
 		}
 
-		/*if ruta.Seguro {
-			jwtMiddleware := jwtm.New(jwtm.Options{
-				ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
-					return []byte("Secret"), nil
-				},
-				SigningMethod: jwt.SigningMethodHS256,
-			})
-			authMiddleware := negroni.New(
-				negroni.HandlerFunc(jwtMiddleware.HandlerWithNext),
-				negroni.Wrap(loggingHandler),
-			)
-			rutaMux.Handler(authMiddleware)
+		if ruta.Seguro {
+			setJwtMiddleware(rutaMux, loggingHandler)
 		} else {
 			rutaMux.Handler(loggingHandler)
 		}
@@ -91,4 +38,27 @@ func New(controladorSolicitud *controlador.Solicitud,
 
 	return router
 }
-*/
+
+func setHeaders(rutaMux *mux.Route) {
+	rutaMux.Headers("Content-Type", "application/json")
+	rutaMux.Headers("Access-Control-Allow-Origin", "*")
+	rutaMux.Headers("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE, OPTIONS")
+	rutaMux.Headers("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+}
+
+func setJwtMiddleware(rutaMux *mux.Route, wrap http.Handler) {
+	if true {
+		return
+	}
+	jwtMiddleware := jwtm.New(jwtm.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return []byte("Secret"), nil
+		},
+		SigningMethod: jwt.SigningMethodHS256,
+	})
+	authMiddleware := negroni.New(
+		negroni.HandlerFunc(jwtMiddleware.HandlerWithNext),
+		negroni.Wrap(wrap),
+	)
+	rutaMux.Handler(authMiddleware)
+}
