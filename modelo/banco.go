68115cc63e76387254ba184a03fbfe17ea18acb2
Guillermo Galizzi <galizzi.guillermo@gmail.com>
2016-11-08 07:17:57 -0300

    Se carga bancos desde json.

--- modelo/banco.go
+++ modelo/banco.go
@@ -5,9 +5,9 @@ import "gopkg.in/mgo.v2/bson"
 type Banco struct {
 	Id          bson.ObjectId `json:"id" bson:"_id"`
 	ProvinciaId bson.ObjectId `json:"provincia" bson:"provinciaId"`
-	CiudadId    bson.ObjectId `json:"ciudad" bson:"ciudadId"`
 	Lat         float64       `json:"lat" bson:"lat"`
 	Lon         float64       `json:"lon" bson:"lon"`
+	Ciudad      string        `json:"ciudad" bson:"ciudad"`
 	Institución string        `json:"nombre" bson:"nombre"`
 	Dirección   string        `json:"direccion" bson:"direccion"`
 	Teléfono    string        `json:"telefono" bson:"telefono"`
@@ -16,13 +16,24 @@ type Banco struct {
 
 type Bancos []Banco
 
-func NewBanco(provinciaId, ciudadId bson.ObjectId,
-	lat, lon float64, institución, dirección, teléfono, horario string) *Banco {
+func NewBanco(provinciaId bson.ObjectId,
+	lat, lon float64, ciudad, institución, dirección, teléfono, horario string) *Banco {
 	return &Banco{
 		bson.NewObjectId(),
 		provinciaId,
-		ciudadId,
 		lat, lon,
-		institución, dirección, teléfono, horario,
+		ciudad, institución, dirección, teléfono, horario,
 	}
+}
+
+func (b *Banco) GetId() bson.ObjectId {
+	return b.Id
+}
+
+func (b *Banco) SetIdHex(hex string) {
+	b.Id = bson.ObjectIdHex(hex)
+}
+
+func (b *Banco) SetId(id bson.ObjectId) {
+	b.Id = id
 }
--- mongo_scripts/carga_datos.js
+++ mongo_scripts/carga_datos.js
@@ -3,9 +3,11 @@ var db = conn.getDB("donemos");
 
 var localidadesC = db.getCollection("localidades");
 var provincias  = db.getCollection("provincias");
+var bancos = db.getCollection("bancos")
 
 localidadesC.drop();
 provincias.drop();
+bancos.drop();
 
 var json = JSON.parse(cat(`${pwd()}/mongo_scripts/provincias_con_localidades.json`));
 
@@ -30,4 +32,26 @@ for (var i = 0; i < json.length; i++) {
       nombre: localidad.nombre
     });
   }
+}
+
+print("Provincias y ciudades cargadas\n")
+
+var json = JSON.parse(cat(`${pwd()}/mongo_scripts/bancos.json`));
+
+for (var i = 0; i < json.length; i++) {
+  var banco = json[i];
+  printjson(banco.institucion);
+  
+  var id = ObjectId();
+  bancos.insert({
+    _id: id,
+    provinciaId: ObjectId(banco.provincia),
+    ciudad: banco.nombreCiudad,
+    institucion: banco.institucion,
+    direccion: banco.direccion,
+    telefono: banco.telefono,
+    horario: banco.horario,
+    lat: banco.lat,
+    lon: banco.lon
+  });
 }
