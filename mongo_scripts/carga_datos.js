ef729ba585d8ad041153ae0b06c02a69c04b0370
sebaferreras <sebastian.ferreras@gmail.com>
2016-11-13 09:52:15 +0100

    Se agregan los bancos al json. Se junta toda la información en un solo archivo.

--- mongo_scripts/carga_datos.js
+++ mongo_scripts/carga_datos.js
@@ -2,59 +2,86 @@ var conn = new Mongo();
 var db = conn.getDB("donemos");
 
 var localidadesC = db.getCollection("localidades");
-var provincias  = db.getCollection("provincias");
-var bancos = db.getCollection("bancos")
+var provinciasC = db.getCollection("provincias");
+var bancosC = db.getCollection("bancos");
 
 localidadesC.drop();
-provincias.drop();
-bancos.drop();
+provinciasC.drop();
+bancosC.drop();
 
-var json = JSON.parse(cat(`${pwd()}/mongo_scripts/provincias_con_localidades.json`));
-
+var json = JSON.parse(cat(`${pwd()}/mongo_scripts/datos.json`));
 
 for (var i = 0; i < json.length; i++) {
   var provincia = json[i];
 
-  printjson(provincia.nombre);
-  var id = ObjectId();
-  provincias.insert({
-    _id: id,
+  var cantidadLocalidades = provincia.ciudades && provincia.ciudades.length ? provincia.ciudades.length : 0;
+  var cantidadBancos = provincia.bancos && provincia.bancos.length ? provincia.bancos.length : 0;
+  printjson(provincia.nombre + ' | Localidades: ' + cantidadLocalidades + ' | Bancos de Sangre: ' + cantidadBancos);
+  
+  // Carga de provincias
+  var provinciaId = ObjectId();
+  
+  provinciasC.insert({
+    _id: provinciaId,
     nombre: provincia.nombre
   });
 
+  // Carga de localidades
   var localidades = provincia.ciudades;
+  
   for (var y = 0; y < localidades.length; y++) {
     var localidad = localidades[y];
-    printjson(localidad.nombre);
-
+    
     localidadesC.insert({
-      provinciaId: id,
+      provinciaId: provinciaId,
       nombre: localidad.nombre
     });
   }
+
+  // Carga de bancos de sangre  
+  var bancos = provincia.bancos;
+
+  if(provincia.bancos) {
+    for(var y = 0; y < bancos.length; y++) {
+      var banco = bancos[y];
+
+      // Obtenemos la lat y la lon del string coordenadas
+      banco.lat = getLat(banco.coordenadas);
+      banco.lon = getLon(banco.coordenadas);
+
+      var id = ObjectId();
+      bancosC.insert({
+        _id: id,
+        provinciaId: provinciaId,
+        ciudad: procesarString(banco.nombreCiudad),
+        institucion: procesarString(banco.institucion),
+        direccion: procesarString(banco.direccion),
+        telefono: banco.telefono,
+        horario: banco.horario,
+        loc: { type: "Point", coordinates: [ banco.lon, banco.lat ] },
+        lat: banco.lat,
+        lon: banco.lon
+      });
+    }
+
+    bancosC.createIndex( { loc: "2dsphere" } );
+  }
 }
 
-print("Provincias y ciudades cargadas\n")
-
-var json = JSON.parse(cat(`${pwd()}/mongo_scripts/bancos.json`));
-
-for (var i = 0; i < json.length; i++) {
-  var banco = json[i];
-  printjson(banco.institucion);
-  
-  var id = ObjectId();
-  bancos.insert({
-    _id: id,
-    provinciaId: ObjectId(banco.provincia),
-    ciudad: banco.nombreCiudad,
-    institucion: banco.institucion,
-    direccion: banco.direccion,
-    telefono: banco.telefono,
-    horario: banco.horario,
-    loc: { type: "Point", coordinates: [ banco.lon, banco.lat ] },
-    lat: banco.lat,
-    lon: banco.lon
-  });
+// Método que convierte un string a formato Oracion y reemplaza algunas abreviaciones o letras
+function procesarString(texto){
+  return texto.split(' ').map(e=> e.charAt(0).toUpperCase() + e.toLowerCase().slice(1)).join(' ')
+                            .replace(" De ", " de ")
+                            .replace(" A ", " a ")
+                            .replace("Y", "y");
 }
 
-bancos.createIndex( { loc: "2dsphere" } );
+// Método que obtiene la latitud de un string de coordenadas
+function getLat(coordenadas) {
+  return coordenadas.replace(" ", "").split(",")[0];
+}
+
+// Método que obtiene la longitud de un string de coordenadas
+function getLon(coordenadas) {
+  return coordenadas.replace(" ", "").split(",")[1];
+}
